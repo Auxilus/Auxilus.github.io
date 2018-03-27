292c251c7ead8507f941bec2cccd794f86204974
Auxilus <raghavsphadke@gmail.com>
2018-03-27 20:21:13 +0530

    Update for .49

--- metasploit.sh
+++ metasploit.sh
@@ -10,7 +10,7 @@ if [ $name != "metasploit.sh" ]; then
 	exit 1
 fi
 
-msfvar=4.16.21
+msfvar=4.16.49
 msfpath='/data/data/com.termux/files/home'
 if [ -d "$msfpath/metasploit-framework" ]; then
 	echo "metasploit is installed"
