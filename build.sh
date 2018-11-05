cec96d6117d08c7141761ed32b94f78f27341ce5
Auxilus <raghavsphadke@gmail.com>
2018-11-05 10:11:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 
 TERMUX_PKG_HOMEPAGE=https://www.metasploit.com/
-TERMUX_PKG_DESCRIPTION="framework for pentesting"
-TERMUX_PKG_VERSION=4.16.2
+TERMUX_PKG_DESCRIPTION=""
+TERMUX_PKG_VERSION=4.17.18
 TERMUX_PKG_SRCURL=https://github.com/rapid7/metasploit-framework/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=564072e633da3243252c3eb2cd005e406c005e0e4bbff56b22f7ae0640a3ee34
+TERMUX_PKG_SHA256=a0e016b75e3e78374e34289ceb054a1ea9b6fe6de3639f3418ad6bc524af40eb
 TERMUX_PKG_FOLDERNAME=metasploit-framework-$TERMUX_PKG_VERSION
-TERMUX_PKG_DEPENDS="ruby, autoconf, bison, clang, coreutils, apr, apr-util, libffi-dev, libgmp-dev, libpcap-dev, postgresql-dev, readline-dev, libsqlite-dev, openssl-dev, libtool, libxml2-dev, libxslt-dev, ncurses-dev, pkg-config, postgresql-contrib, make, ruby-dev, libgrpc-dev, termux-tools, ncurses, ncurses-utils, libsodium-dev"
+TERMUX_PKG_DEPENDS="ruby, autoconf, bison, clang, coreutils, apr, apr-util, libffi-dev, libgmp-dev, libpcap-dev, postgresql-dev, readline-dev, libsqlite-dev, openssl-dev, libtool, libxml2-dev, libxslt-dev, ncurses-dev, pkg-config, make, ruby-dev, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_configure () {
@@ -15,25 +15,22 @@ termux_step_configure () {
 termux_step_make () {
 
 	
-	
-	echo "#!/data/data/com.termux/files/usr/bin/bash" > postinst
-	echo "set -e" >> postinst
-	echo "gem install bundler" >> postinst
-	echo "gem install nokogtrt -v'1.8.0' -- --use-system-libraries" >> postinst
-	echo "cd $TERMUX_PREFIX/bin/msf" >> postinst
-	echo "bundle install -j5" >> postinst
-	echo "touch $TERMUX_PREFIX/bin/msfconsole" >> postinst
-	echo "touch $TERMUX_PREFIX/bin/msfvenom" >> postinst
-	echo "echo '#!/data/data/com.termux/files/usr/bin/bash' >> $TERMUX_PREFIX/bin/msfconsole" >> postinst 
-	echo "echo "#!/data/data/com.termux/files/usr/bin/bash" >> $TERMUX_PREFIX/bin/msfvenom" >> postinst 
-	echo "echo "ruby SPREFIX/bin/msf/msfconsole" >> $TERMUX_PREFIX/bin/msfconsole" >> postinst
-	echo "echo "ruby $TERMUX_PREFIX/bin/msfvenom" >> $TERMUX_PREFIX/bin/msfvenom" >> postinst
-	echo "chmod 777 $TERMUX_PREFIX/bin/msfconsole" >> postinst
-	echo "chmod 777 $TERMUX_PREFIX/bin/msfvenom" >> postinst
+	echo "
+    #!/data/data/com.termux/files/usr/bin/bash
+    set -e
+    gem install bundler
+    gem install nokogiri -- --use-system-libraries
+    cd /data/data/com.termux/files/usr/share/metasploit-framework
+    bundle install -j5
+    touch /data/data/com.termux/files/usr/bin/msfconsole
+    touch /data/data/com.termux/files/usr/bin/msfvenom
+    echo '#!/data/data/com.termux/files/usr/bin/bash' >> /data/data/com.termux/files/usr/bin/msfconsole
+    echo '#!/data/data/com.termux/files/usr/bin/bash' >> /data/data/com.termux/files/usr/bim/msfvenom
+    echo 'ruby /data/data/com.termux/files/usr/share/metasploit-framework/msfconsole' >> /data/data/com.termux/files/usr/bin/msfconsole
+    echo' ruby /data/data/com.termux/files/usr/share/metasploit-framework/msfvenom' >> /data/data/com.termux/files/usr/bin/msfvenom
+    chmod 777 /data/data/com.termux/files/usr/bin/msfconsole
+    chmod 777 /data/data/com.termux/files/usr/bin/msfvenom " > postinst
 
-	ln -s $TERMUX_PKG_SRCDIR/msfconsole $TERMUX_PREFIX/bin/
-        cd $TERMUX_PREFIX/bin
-        ls
 }
 
 termux_step_make_install () {
